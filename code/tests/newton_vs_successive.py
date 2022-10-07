fc665ec7cb1298d426fd368d195b0cc80bff252b
John Stachurski <john.stachurski@gmail.com>
2022-10-07 13:38:34 +1100

    misc

---
+++ code/tests/newton_vs_successive.py
@@ -0,0 +1,26 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %run ../src/wc_ratio_multi_index.py
+
+s = 18
+ssy = SSY(L=s, K=s, I=s, J=s, build_single_index=False)
+
+# %%time
+w_star_multi_sa = wc_ratio_multi_index(ssy, algorithm="successive_approx")
+
+# %%time
+w_star_multi_newton = wc_ratio_multi_index(ssy, algorithm="newton")
+
+
