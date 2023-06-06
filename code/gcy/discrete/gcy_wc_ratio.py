4be4e7c2225682fede872b1ee61bdafb33deb2db
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-06-06 09:53:43 +0530

    final fix

--- code/gcy/discrete/gcy_wc_ratio.py
+++ code/gcy/discrete/gcy_wc_ratio.py
@@ -268,7 +268,7 @@ def T_gcy2(w, shapes, params, arrays):
      h_c_states,  h_c_Q,  σ_c_states,
      h_zπ_states, h_zπ_Q, σ_zπ_states,
      h_λ_states,  h_λ_Q)   = arrays
-
+    # print("line -271", z_π_Q.shape)
     θ = (1 - γ) / (1 - 1/ψ)
     n = len(shapes)
 
@@ -278,17 +278,25 @@ def T_gcy2(w, shapes, params, arrays):
                       'h_z'    : 2,
                       'h_c'    : 3,
                       'h_zπ'   : 4,
-                      'h_λ'    : 5}
+                      'h_λ'    : 5,
+                      'jz'      : 6,
+                      'jz_π'    : 7,
+                      'jh_z'    : 8,
+                      'jh_c'    : 9,
+                      'jh_zπ'   : 10,
+                      'jh_λ'    : 11}
     #w = np.reshape(w, (1, 1, 1, 1, 1, 1, n_z, n_z_π, n_h_z, n_h_c, n_h_zπ, n_h_λ))
     w = np.expand_dims(w, (0,1,2,3,4,5))
     # (0,1,2,3,4,5,6,7,8,9,10,11)
     # Create intermediate arrays
+    ms = {0,1,2,3,4,5,6,7,8,9,10,11}
     A1 = np.exp(θ * h_λ_states)
     indices_a = [1] * n
     indices_b = [1] * n
     indices_b[state_numbers['h_λ']] = n_h_λ
     indices = indices_a + indices_b
-    A1= np.expand_dims(A1, (0,1,2,3,4,5,6,7,8,9,10))
+    fs = {state_numbers['jh_λ']}
+    A1= np.expand_dims(A1, list(ms-fs))
     # A1 = np.reshape(A1, indices)
 
     A2 = np.exp(0.5 * ((1 - γ) * σ_c_states)**2)
@@ -296,7 +304,8 @@ def T_gcy2(w, shapes, params, arrays):
     indices_b = [1] * n
     indices_a[state_numbers['h_c']] = n_h_c
     indices = indices_a + indices_b
-    A2= np.expand_dims(A2, (0,1,2,4,5,6,7,8,9,10,11))
+    fs = {state_numbers['h_c']}
+    A2= np.expand_dims(A2, list(ms-fs))
     #A2 = np.reshape(A2, indices)
 
     A3 = np.exp((1 - γ) * (μ_c + z_states))
@@ -313,36 +322,42 @@ def T_gcy2(w, shapes, params, arrays):
     indices_a[state_numbers['h_z']] = n_h_z
     indices_a[state_numbers['h_zπ']] = n_h_zπ
     indices_a[state_numbers['z']] = n_z
+    fs = {state_numbers['z_π'], state_numbers['h_z'],state_numbers['h_zπ'],
+          state_numbers['z'] }
     indices = indices_a + indices_b
     A3 = np.swapaxes(A3, 2, 3)
     A3 = np.swapaxes(A3, 1, 2)
     A3 = np.swapaxes(A3, 0, 1)
-    A3= np.expand_dims(A3, (3,5,6,7,8,9,10,11))
+    A3= np.expand_dims(A3, list(ms-fs))
     #A3 = np.reshape(A3, indices)
 
     # Reshape h_z_Q
     indices = [1] * n
     indices[state_numbers['h_z']] = n_h_z
+    fs = {state_numbers['h_z'], state_numbers['jh_z']}
     # h_z_Q = np.reshape(h_z_Q, indices + indices) # (0,1,2,3,4,5,6,7,8,9,10,11)
-    h_z_Q= np.expand_dims(h_z_Q, (0,1,3,4,5,6,7,9,10,11))
+    h_z_Q= np.expand_dims(h_z_Q, list(ms-fs))
 
     # Reshape h_zπ_Q
     indices = [1] * n
     indices[state_numbers['h_zπ']] = n_h_zπ
+    fs = {state_numbers['h_zπ'], state_numbers['jh_zπ']}
     #h_zπ_Q = np.reshape(h_zπ_Q, indices + indices)
-    h_zπ_Q= np.expand_dims(h_zπ_Q, (0,1,2,3,5,6,7,8,9,11))
+    h_zπ_Q= np.expand_dims(h_zπ_Q, list(ms-fs))
 
     # Reshape h_λ_Q
     indices = [1] * n
     indices[state_numbers['h_λ']] = n_h_λ
     #h_λ_Q = np.reshape(h_λ_Q, indices + indices)
-    h_λ_Q= np.expand_dims(h_λ_Q, (0,1,2,3,4,6,7,8,9,10))
+    fs = {state_numbers['h_λ'], state_numbers['jh_λ']}
+    h_λ_Q= np.expand_dims(h_λ_Q, list(ms-fs))
 
     # Reshape h_c_Q
     indices = [1] * n
     indices[state_numbers['h_c']] = n_h_c
+    fs = {state_numbers['h_c'], state_numbers['jh_c']}
     #h_c_Q = np.reshape(h_c_Q, indices + indices)
-    h_c_Q= np.expand_dims(h_c_Q, (0,1,2,4,5,6,7,8,10,11))
+    h_c_Q= np.expand_dims(h_c_Q, list(ms-fs))
 
     # state_numbers = { 'z'      : 0,
     #                   'z_π'    : 1,
@@ -356,11 +371,11 @@ def T_gcy2(w, shapes, params, arrays):
     indices_a[state_numbers['z_π']] = n_z_π
     indices_b = [1] * n
     indices_b[state_numbers['z_π']] = n_z_π
+    fs = {state_numbers['h_zπ'], state_numbers['z_π'], state_numbers['jz_π']}
+
     #z_π_Q = np.reshape(z_π_Q, indices_a + indices_b)
     z_π_Q = np.swapaxes(z_π_Q, 0, 1)
-    z_π_Q= np.expand_dims(z_π_Q, (0,2,3,5,6,8,9,10,11))
-
-    #breakpoint()
+    z_π_Q= np.expand_dims(z_π_Q, list(ms-fs))
 
     # Reshape z_Q[i_z_π, i_h_z, i_h_zπ, i_z, j_z]
     indices_a = [1] * n
@@ -370,6 +385,9 @@ def T_gcy2(w, shapes, params, arrays):
     indices_a[state_numbers['z']] = n_z
     indices_b = [1] * n
     indices_b[state_numbers['z']] = n_z
+    fs = {state_numbers['z_π'], state_numbers['h_z'],
+          state_numbers['h_zπ'], state_numbers['z'],
+          state_numbers['jz']}
     # state_numbers = { 'z'      : 0,
     #                   'z_π'    : 1,
     #                   'h_z'    : 2,
@@ -380,15 +398,16 @@ def T_gcy2(w, shapes, params, arrays):
     z_Q = np.swapaxes(z_Q, 1, 2)
     z_Q = np.swapaxes(z_Q, 0, 1)
     # changed to z_Q[i_z, i_z_π, i_h_z, i_h_zπ, j_z]
-    print("line 363 z_Q", z_Q.shape, z_Q[0, 1, 1, 1])
-    z_Q = np.reshape(z_Q, indices_a + indices_b)
-    #z_Q= np.expand_dims(z_Q, (3,5,7,8,9,10,11))
-    print("line 366 z_Q", z_Q.shape, z_Q[0, 1, 1, 0, 1, 0, :,0,0,0,0,0])
+    # print("line 363 z_Q", z_Q.shape, z_Q[0, 1, 1, 1])
+    #z_Q = np.reshape(z_Q, indices_a + indices_b)
+    z_Q= np.expand_dims(z_Q, list(ms-fs))
+    # print("line 366 z_Q", z_Q.shape, z_Q[0, 1, 1, 0, 1, 0, :,0,0,0,0,0])
     #pprint.pprint(z_Q)
     # Take product and sum along last six axes
-    H = A1 * A2 * A3 * h_z_Q * h_zπ_Q * h_λ_Q * h_c_Q * z_π_Q * z_Q
+    # n_z, n_z_π, n_h_z, n_h_c, n_h_zπ, n_h_λ
+    H = A1 * A2 * A3 * z_Q * h_z_Q * h_c_Q * h_zπ_Q * h_λ_Q  * z_π_Q
     Hwθ = np.sum(w**θ * H, axis=(6, 7, 8, 9, 10, 11))
-    print(Hwθ[0,1,1,0,1, 1], "vec Hw")
+    # print(Hwθ[0,1,1,0,1, 1], "vec Hw")
     # i_z = 0, i_z_π=1, i_h_z=1, i_h_c=0, i_h_zpi=1, i_h_lambda=1
     #w: n_z, n_z_π, n_h_z, n_h_c, n_h_zπ, n_h_λ
     #Hwθ = jnp.sum(w**θ * H, axis=(0, 1, 2, 3, 4, 5))
@@ -420,7 +439,7 @@ def T_gcy_loops(w, shapes, params, arrays):
      h_c_states,  h_c_Q,  σ_c_states,
      h_zπ_states, h_zπ_Q, σ_zπ_states,
      h_λ_states,  h_λ_Q)   = arrays
-
+    # print("line -442", z_π_Q.shape, n_z)
     θ = (1 - γ) / (1 - 1/ψ)
     Hwθ = np.empty(shapes)
     foo=[]
@@ -428,7 +447,7 @@ def T_gcy_loops(w, shapes, params, arrays):
         for i_h_c in range(n_h_c):
             for i_h_zπ in range(n_h_zπ):
                 for i_h_λ in range(n_h_λ):
-                    for i_z_π in range(n_z):
+                    for i_z_π in range(n_z_π):
                         for i_z in range(n_z):
                             Hwθ_sum = 0.0
                             z = z_states[i_z_π, i_h_z, i_h_zπ, i_z]
@@ -436,7 +455,7 @@ def T_gcy_loops(w, shapes, params, arrays):
                             a2 = np.exp(0.5 * ((1 - γ) * σ_c)**2)
                             a3 = np.exp((1 - γ) * (μ_c + z))
                             for j_z in range(n_z):
-                                for j_z_π in range(n_z):
+                                for j_z_π in range(n_z_π):
                                     for j_h_z in range(n_h_z):
                                         for j_h_c in range(n_h_c):
                                             for j_h_zπ in range(n_h_zπ):
@@ -455,45 +474,45 @@ def T_gcy_loops(w, shapes, params, arrays):
                                                             w[j_z, j_z_π, j_h_z, j_h_c, j_h_zπ, j_h_λ]**θ * \
                                                             p * a
 
-                                                        if i_z==0 and i_z_π==1 and i_h_z==1 and i_h_c==0 and i_h_zπ==1 and i_h_λ==1:
-                                                            foo.append(w[j_z, j_z_π, j_h_z, j_h_c, j_h_zπ, j_h_λ]**θ *p * a)
-                                                            # print("-----")
-                                                            # print("p0", p0)
-                                                            # print("p1", p1)
-                                                            # print("p2", p2)
-                                                            # print("p3", p3)
-                                                            # print("p4", p4)
-                                                            # print("p5", p5)
-                                                            # print("a1", a1)
-                                                            # print("a2", a2)
-                                                            # print("a3", a3)
-                                                            # print("w", w[j_z, j_z_π, j_h_z, j_h_c, j_h_zπ, j_h_λ])
-                                                            # print("-----")
-                                                            if j_z + j_z_π + j_h_z + j_h_c + j_h_zπ + j_h_λ == 0:
-                                                                print("-----")
-                                                                print("p0", p0)
-                                                                print("p1", p1)
-                                                                print("p2", p2)
-                                                                print("p3", p3)
-                                                                print("p4", p4)
-                                                                print("p5", p5)
-                                                                print("a1", a1)
-                                                                print("a2", a2)
-                                                                print("a3", a3)
-                                                                print("w", w[j_z, j_z_π, j_h_z, j_h_c, j_h_zπ, j_h_λ]**θ )
-                                                                print("-----")
+                                                        # if i_z==0 and i_z_π==1 and i_h_z==1 and i_h_c==0 and i_h_zπ==1 and i_h_λ==1:
+                                                        #     foo.append(w[j_z, j_z_π, j_h_z, j_h_c, j_h_zπ, j_h_λ]**θ *p * a)
+                                                        #     # print("-----")
+                                                        #     # print("p0", p0)
+                                                        #     # print("p1", p1)
+                                                        #     # print("p2", p2)
+                                                        #     # print("p3", p3)
+                                                        #     # print("p4", p4)
+                                                        #     # print("p5", p5)
+                                                        #     # print("a1", a1)
+                                                        #     # print("a2", a2)
+                                                        #     # print("a3", a3)
+                                                        #     # print("w", w[j_z, j_z_π, j_h_z, j_h_c, j_h_zπ, j_h_λ])
+                                                        #     # print("-----")
+                                                        #     if j_z + j_z_π + j_h_z + j_h_c + j_h_zπ + j_h_λ == 0:
+                                                        #         print("-----")
+                                                        #         print("p0", p0)
+                                                        #         print("p1", p1)
+                                                        #         print("p2", p2)
+                                                        #         print("p3", p3)
+                                                        #         print("p4", p4)
+                                                        #         print("p5", p5)
+                                                        #         print("a1", a1)
+                                                        #         print("a2", a2)
+                                                        #         print("a3", a3)
+                                                        #         print("w", w[j_z, j_z_π, j_h_z, j_h_c, j_h_zπ, j_h_λ]**θ )
+                                                        #         print("-----")
                             Hwθ[i_z, i_z_π, i_h_z, i_h_c, i_h_zπ, i_h_λ] = Hwθ_sum
-
+                            assert Hwθ_sum >0.0
 
     # Define and return Tw
     #Hwθ = Hwθ*w**θ
-    print(Hwθ[0,1,1,0,1, 1],"loop Hw")
+    # print(Hwθ[0,1,1,0,1, 1],"loop Hw")
     Tw = 1 + β * Hwθ**(1/θ)
     #pprint.pprint(foo)
     return Tw
 
-
-def test_vectorized_equals_loops(shapes=(2, 2, 2, 2, 2, 2)):
+# fails: 2, 3, 3, 2, 3, 2
+def test_vectorized_equals_loops(shapes=(3, 4, 5, 2, 6, 7)):
     """
     Test that T generated by vectorized logic produces the same result as T
     generated by loops.  Evaluate at a random choice of w.
@@ -502,8 +521,10 @@ def test_vectorized_equals_loops(shapes=(2, 2, 2, 2, 2, 2)):
     params = gcy.params
     arrays = discretize_gcy(gcy, shapes)
     w = np.exp(np.random.randn(*shapes))  # Test operator at w
-    w1 = T_gcy2(w, shapes, params, arrays)
+    t = w.copy()
+
     w2 = T_gcy_loops(w, shapes, params, arrays)
+    w1 = T_gcy2(w, shapes, params, arrays)
     print(np.allclose(w1,w2))
 
 
