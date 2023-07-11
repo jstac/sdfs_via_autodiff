cdd02764fa3e19798dab96132d27730a9d84fbda
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-07-11 13:00:36 +0530

    fix mean in z_grid

--- code/gcy/continuous/gcy_wc_ratio_continuous.py
+++ code/gcy/continuous/gcy_wc_ratio_continuous.py
@@ -62,8 +62,8 @@ def build_grid(gcy,
     # z' = ρ * z + ρ_π * z_π + σ_z * η0
     h_z_max = num_std_devs * jnp.sqrt(s_z**2 / (1 - ρ_z**2))
     σ_z_max = φ_z * jnp.exp(h_z_max)
-    z_max = zπ_grid[-1] + num_std_devs * σ_z_max
-    z_min = zπ_grid[0] - num_std_devs * σ_z_max
+    z_max = zπ_grid[-1] / (1 - ρ) + num_std_devs * σ_z_max
+    z_min = zπ_grid[0] / (1 - ρ) - num_std_devs * σ_z_max
     z_grid = jnp.linspace(z_min, z_max, z_grid_size)
 
     return h_λ_grid, h_c_grid, h_z_grid, h_zπ_grid, z_grid, zπ_grid
@@ -486,3 +486,5 @@ def compare_T_factories(T_fact_old, T_fact_new, shape=(2, 3, 4, 5, 6, 7),
     comparison_result = jnp.asarray([jnp.allclose(*w1) for w1 in
                                      zip(w1_old_list, w1_new_list)])
     print("Same results? {}".format(jnp.all(comparison_result)))
+
+print(wc_ratio_continuous(GCY()))
